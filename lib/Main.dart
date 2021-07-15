695bf91250340f055bcdac3b5a238381dc76ff0e
MSaeed <mahmoudsaeed1996@gmail.com>
2021-07-15 17:01:33 +0200

    * Add app icon * Change placeholder image * Change app label for android

--- lib/Main.dart
+++ lib/Main.dart
@@ -3,6 +3,7 @@ import '/screens/HomeScreen.dart';
 import '/screens/ArticleDetailsScreen.dart';
 
 void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/home',
       routes: {
         '/home': (context) => Home(),
